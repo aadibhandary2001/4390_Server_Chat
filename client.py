d056c50322a38e4d05ced73d6d782b6c68d443bf
aadibhandary2001 <aadibhandary2001@gmail.com>
2022-04-06 16:35:30 -0500

    Created template client that can send messages to server until user types 'EXIT'

--- client.py
+++ client.py
@@ -15,12 +15,12 @@ import sys
 HOST=socket.gethostbyname(sys.argv[1]) #Get domain name and IP from command line
 PORT=int(sys.argv[2]) #Get port from command line
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    exitTok="EXIT"
-    for line in sys.stdin:
-        if exitTok == line.rstrip():
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #Try to create socket nd define as s
+    s.connect((HOST, PORT)) #Connect To Port
+    exitTok="EXIT" #Set Exit Token
+    for line in sys.stdin: #Read line inputs from user indefinitely
+        if exitTok == line.rstrip(): #If exitTok, exit for loop
             break
-        s.sendall(line.encode())
-        data = s.recv(1024)
-        print(f"Received {data!r}")
+        s.sendall(line.encode()) #Encode data and send byte stream
+        data = s.recv(1024) #Receive back to a buffer of 1024
+        print(f"Received {data!r}") #Print Received Result
