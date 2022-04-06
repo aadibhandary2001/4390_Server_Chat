a418c47dd24d952fe88ea94a668848df97624e5a
aadibhandary2001 <aadibhandary2001@gmail.com>
2022-04-06 10:31:05 -0500

    Added Print Line after receiving data

--- server.py
+++ server.py
@@ -30,6 +30,7 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
         print(f"Connected by {addr}")
         while True:
             data=conn.recv(1024)
+            print(data)
             newData=data[::-1]
             if not data:
                 break
