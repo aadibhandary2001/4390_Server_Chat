9f271baa800c8312b750fedf491857e2ed105801
BMcFarlane77 <Brycemcfarlane7@gmail.com>
2022-04-11 21:01:11 -0500

    Added Encryption to the client side

--- client.py
+++ client.py
@@ -10,6 +10,10 @@
 #File Resources and Imports Below
 import socket
 import sys
+import Encryptor
+
+# Creation of the test Encryptor. All data sent or received passes through this.
+Authenticator = Encryptor.Cryptographer(b'test_key', b'test_salt')
 
 #1. Connect to Server
 HOST=socket.gethostbyname(sys.argv[1]) #Get domain name and IP from command line
@@ -21,6 +25,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #Try to create sock
     for line in sys.stdin: #Read line inputs from user indefinitely
         if exitTok == line.rstrip(): #If exitTok, exit for loop
             break
-        s.sendall(line.encode()) #Encode data and send byte stream
-        data = s.recv(1024) #Receive back to a buffer of 1024
+        s.sendall(Authenticator.encrypt(line)) #Encrypt data and send byte stream
+        data = Authenticator.decrypt(s.recv(1024)) #Receive back to a buffer of 1024
         print(f"Received {data!r}") #Print Received Result
