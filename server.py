bfd9aedc944d8f9482decd35520edf0dbf09cd10
UTDDavid <91843300+UTDDavid@users.noreply.github.com>
2022-04-13 00:48:49 -0500

    Added HELLO Client-ID to login or create account
    The client can now type "Hello Client-ID" to login or to create a new account.
    Has unknown bug where sometimes client randomly prints "\x05\x05\x05\x05\x05" and freezes after receiving data from server when logging in.

--- server.py
+++ server.py
@@ -19,6 +19,58 @@ import Encryptor
 
 Authenticator = Encryptor.Cryptographer(b'test_key', b'test_salt')
 
+usernames = ["dababy", "pog"]
+passwords = ["Apple", "Banana"]
+
+# HELLO (Client-ID-A) Protocal Message
+def hello(newCon, data):
+    # Client Username
+    clientID = data.split()[1]
+
+    # Username exists
+    if clientID in usernames:
+        # Asks for password
+        greetUser = "User_Exists "
+        greetUser += " Username: " + clientID
+        greetUser += " Please enter your password: "
+        newCon.sendall(Authenticator.encrypt(greetUser))
+        
+        # Get client password
+        data=Authenticator.decrypt(newCon.recv(1024))
+        print("Says: ",data)
+        index = usernames.index(clientID)
+        
+        # Correct password
+        if passwords[index] == data:
+            loginSucess = "Sucessfully Logged in"
+            newCon.sendall(Authenticator.encrypt(loginSucess))
+
+        # Wrong password
+        else:
+            loginFailure = "Wrong_Password: Please try logging in again"
+            newCon.sendall(Authenticator.encrypt(loginFailure))
+
+    # Username doesn't exist
+    else:
+        # Adds clientID to usernames list
+        usernames.append(clientID)
+        
+        # Asks to create password
+        createPasswordMsg = "User_Not_Found"
+        createPasswordMsg += "User '" + clientID + "' is not on the list of subscribers"
+        createPasswordMsg += "Please create a password for the new user: "
+        newCon.sendall(Authenticator.encrypt(createPasswordMsg))
+        
+        # place new user password into list
+        data=Authenticator.decrypt(newCon.recv(1024))
+        print("Says: ",data)
+        index = usernames.index(clientID)
+        passwords.append(data)
+        
+        newUserSucess = "Sucessfully Created Account for User: " + clientID
+        newUserSucess += "Please try logging into your new account to chat"
+        newCon.sendall(Authenticator.encrypt(newUserSucess))
+
 #Utility Functions
 def handleClient(newCon,newAddr): #Handle client. Threadded function for concurrent client handling
     with newCon:
@@ -29,7 +81,13 @@ def handleClient(newCon,newAddr): #Handle client. Threadded function for concurr
                 break
             print(newAddr,"Says: ",data) #print client input
             newData=data[::-1]  #reverse client input
-            newCon.sendall(Authenticator.encrypt(newData))#else, we return values to the client
+            
+            # connected client tries to log on by sending "HELLO Client-Username"
+            first_word = data.split()[0]
+            if first_word == "HELLO":
+            	hello(newCon, data)
+
+            newCon.sendall(Authenticator.encrypt(newData)) #else, we return values to the client
 
 #Code Below Sets up welcome socket
 HostName=socket.gethostname() #Obtain Host Name
